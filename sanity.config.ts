b8a145e8875de52f28575e20bdc197335d9ece35
antithetic <ego.war@icloud.com>
2025-05-20 21:20:16 -0700

    create Workspaces config, install Lucide

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,6 +3,14 @@ import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './src/schemaTypes'
 
+import {Bone, FlipHorizontal, PocketKnife, ScanHeart} from 'lucide-react'
+
+
+import {consentSchemaTypes} from './src/schemaTypes/consent'
+import {filosoSchemaTypes} from './src/schemaTypes/filoso'
+import {loveSchemaTypes} from './src/schemaTypes/love-hangover'
+import {mirrorSchemaTypes} from './src/schemaTypes/mirror'
+
 const sharedConfig = {
   projectId: 'ioq9oqcu',
   dataset: 'production',
@@ -12,16 +20,49 @@ const sharedConfig = {
   ],
 }
 
-export default defineConfig({
-  name: 'default',
-  title: 'Consent Press Studio',
-
-  projectId: 'ioq9oqcu',
-  dataset: 'production',
-
-  plugins: [structureTool(), visionTool()],
-
-  schema: {
-    types: schemaTypes,
+export default defineConfig([
+  {
+    name: 'consent',
+    title: 'Consent Press',
+    ...sharedConfig,
+    icon: Bone,
+    plugins: [...sharedConfig.plugins],
+    schema: {
+      types: consentSchemaTypes,
+    },
+    basePath: '/consent',
   },
-})
+  {
+    name: 'filoso',
+    title: 'Filoso',
+    ...sharedConfig,
+    icon: PocketKnife,
+    plugins: [...sharedConfig.plugins],
+    schema: {
+      types: filosoSchemaTypes,
+    },
+    basePath: '/links',
+  },
+  {
+    name: 'love',
+    title: 'Love Hangover',
+    ...sharedConfig,
+    icon: ScanHeart,
+    plugins: [...sharedConfig.plugins],
+    schema: {
+      types: loveSchemaTypes,
+    },
+    basePath: '/love',
+  },
+  {
+    name: 'mirror',
+    title: 'Mirror',
+    ...sharedConfig,
+    icon: FlipHorizontal,
+    plugins: [...sharedConfig.plugins],
+    schema: {
+      types: mirrorSchemaTypes,
+    },
+    basePath: '/mirror',
+  },
+])

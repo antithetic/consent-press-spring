f7df746335565307c977ce14b460f1a19d5c0012
antithetic <ego.war@icloud.com>
2025-05-21 15:16:28 -0700

    add profileType

--- src/schemaTypes/links/index.ts
+++ src/schemaTypes/links/index.ts
@@ -1,1 +1,15 @@
-export const linksSchemaTypes = []
+import { defineField, defineType } from 'sanity'
+
+export const profileType = defineType({
+    name: 'profile',
+    title: 'Profile',
+    type: 'document',
+    fields: [
+      defineField({
+        name: 'name',
+        type: 'string',
+      }),
+    ],
+  })
+
+export const linksSchemaTypes = [ profileType ]
